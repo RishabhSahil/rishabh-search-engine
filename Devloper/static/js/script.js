52a872af56299414b06ae23c5cf9713f3bff78b9
--v--: ᏒᎥShᎪbH :--v-- <91429171+RishabhSahil@users.noreply.github.com>
2022-12-11 23:35:47 +0530

    Add files via upload

--- Devloper/static/js/script.js
+++ Devloper/static/js/script.js
@@ -147,3 +147,18 @@ document.getElementById('frmsearch').onsubmit = function() {
     }
 
 }
+
+function ics(){
+    query = document.getElementById('textSearch').value;
+    if(query==false){
+        
+        console.log("please enter your query");
+        window.location = 'https://rishabhsahil.github.io/ics/'
+        return false;
+    }
+    else{
+        window.location = 'https://www.google.com/search?q=' + query
+        return false;
+
+    }
+} 
